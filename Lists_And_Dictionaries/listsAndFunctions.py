b64c4c7eb4daf1005a119a74c7c340883322b3cc
Carlos Nunez <carlosnunez114@gmail.com>
2024-11-29 17:55:33 -0500

    list and functions cont

--- Lists_And_Dictionaries/listsAndFunctions.py
+++ Lists_And_Dictionaries/listsAndFunctions.py
@@ -36,3 +36,52 @@ def first_item_in_list(item):
     return f"This is the first item: {item[0]}"
 
 print(first_item_in_list(n))
+
+# modifying a list in a function modifies the original list as well
+
+# def multiply_index(list):
+#     i = 0
+#     while i < len(list):
+#         list[i] = list[i] * 2
+#         i +=1
+#     return list
+
+generic_list = [3,5,7]
+# print (multiply_index(generic_list))
+
+def multiply_index(list):
+    for i in range(0, len(list)):
+        list[i] = list[i] * 2
+    print(list)
+
+multiply_index(generic_list) # does the same as the while function above
+
+def append_list(lst):
+    lst.append(18)
+    return lst
+
+print(append_list(generic_list))
+
+def print_each_list_index(x):
+    for i in range(0, len(x)):
+        print(x[i])
+
+print_each_list_index(generic_list)
+
+# The range function has three different versions:
+
+# range(stop)
+# range(start, stop)
+# range(start, stop, step)
+# In all cases, the range() function returns a list of numbers from start up to (but not including) stop. Each item increases by step.
+
+# If omitted, start defaults to 0 and step defaults to 1.
+
+
+#!does not work
+# def create_number_list(x):
+#     for i in range(0, len(x)):
+#         x[i] = x[i]
+#     return x
+
+# print (create_number_list(range(3)))
