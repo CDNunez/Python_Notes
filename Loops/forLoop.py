02bd07f4a76994a4d50d94ca881a5bf1e87d3ec2
Carlos Nunez <carlosnunez114@gmail.com>
2024-12-17 19:33:20 -0500

    For Loops

---
+++ Loops/forLoop.py
@@ -0,0 +1,55 @@
+for i in range(11):
+    print(i)
+
+# for loop to append to a list
+# to_append = []
+# # range depending however many  items you want to add to list
+# for i in range(5):
+#     item_to_add=input("User input")
+#     to_append.append(item_to_add)
+# print(to_append)
+
+# for loop iterating thru strings
+example_string = "string"
+for character in example_string:
+    print(character)
+
+#* Practice exercise not reflective of current lesson
+# db = []
+
+# username = input("username: ")
+# password = input('password: ')
+
+# def add_to_db(username,password):
+#     user = []
+#     user.append(username)
+#     user.append(password)
+#     db.append(user)
+
+# add_to_db(username,password)
+
+# def verify_user(db):
+#     for user in db:
+#         if user[0] == username and user[1] == password:
+#             print("New user created", db)
+#         else:
+#             print("I dont know where I was going with this func")
+
+# verify_user(db)
+
+# string manipulation in for loops
+# phrase = "Don't beat around the bush"
+
+# for char in phrase:
+#     if char == "O" or char == "o":
+#         print("X"),
+#     else:
+#         print(char),
+# print
+
+# looping thru a list
+
+shonen = ["Jojo's Bizzare Adventrue", "Dragonball Z", "Pokemon", "Yu-Gi-Oh"]
+
+for anime in shonen:
+    print(anime)
